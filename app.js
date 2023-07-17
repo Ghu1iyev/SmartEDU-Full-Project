b3d6b1c803ef5648325561c2c01b9754b02cc08e
Ghu1iyev <tu12n4c1r@code.edu.az>
2023-07-17 17:27:27 +0400

    list the courses

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
+
+
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('DB Connected Succesfuly');
+}).catch((err) => {
+    console.log(err);
+})
 
 //Template Engine
 app.set("view engine", "ejs")
 
 //Middlewares
 app.use(express.static('public'))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.use('/', pageRoute)
+app.use('/courses', courseRoute)
 
 
 const port = 3000
